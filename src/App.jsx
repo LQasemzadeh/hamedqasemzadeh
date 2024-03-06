08fbbe18c1c32eac80f1c5d02ff2b90b4dfcc1e7
lqasemzadeh <lqasemzadeh@gmail.com>
2024-03-06 10:09:53 +0330

    craete main projrct

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import NavbarComponent from "./components/ui/Navbar.Component";
+import FooterComponent from "./components/ui/Footer.Component";
+
+
+function App() {
+  return (
+    <div>
+      { /* Navbar */ }
+      <NavbarComponent />
+
+      { /* Wrapper */ }
+      <div className="wrapper">
+
+      </div>
+
+      { /* Footer */ }
+      <FooterComponent />
+
+    </div>
+  );
+}
+
+export default App;
